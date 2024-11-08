fdef44e426c7e1f9394c1fa5851b3f8b12073d9f
Jari Peeperkorn <jari.peeperkorn@gmail.com>
2024-11-08 13:45:05 +0100

    cleaned up training code

--- DP_OOPPM/train_model.py
+++ DP_OOPPM/train_model.py
@@ -66,27 +66,20 @@ def train_and_return_LSTM(X_train, y_train, s_train, loss_function,
     #if fair loss we also use bce as balance
     criterion_bce = torch.nn.BCELoss()
 
-    #! decide if we want to use NAdam or not, check what state of art is now
-
     # Optimizer and scheduler used by benchmark
-    optimizer = torch.optim.NAdam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
     lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode='min', factor=0.75, patience=10, threshold=0.01,
+        optimizer, mode='min', factor=0.75, patience=10, threshold=0.001,
         threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08
     )
 
-    # Create DataLoader for training
-    train_dataset = TensorDataset(X_train, y_train, s_train)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
-    # Create DataLoader for validation if data is provided
-    if X_val is not None and y_val is not None and s_val is not None:
-        val_dataset = TensorDataset(X_val, y_val, s_val)
-        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)    
+    # Create DataLoader for training and validation
+    train_loader, val_loader = prepare_data_loaders(X_train, y_train, s_train, X_val, y_val, s_val, batch_size)
 
     # Early stopping variables
     best_val_loss = np.inf
     epochs_without_improvement = 0
+    early_stopping = False
 
     losses = []
     val_losses = []
@@ -94,8 +87,14 @@ def train_and_return_LSTM(X_train, y_train, s_train, loss_function,
     # Store the best model state
     best_model_state = None
 
+    #load data to device only once
+    X_train, y_train, s_train = X_train.to(device), y_train.to(device), s_train.to(device)
+    if X_val is not None and y_val is not None and s_val is not None:
+        X_val, y_val, s_val = X_val.to(device), y_val.to(device), s_val.to(device)
 
     for epoch in range(max_epochs):
+        if early_stopping:
+            break
         # Set model to training mode each epoch
         model.train()
 
@@ -103,20 +102,15 @@ def train_and_return_LSTM(X_train, y_train, s_train, loss_function,
 
         # Training loop over each batch
         for X_batch, y_batch, s_batch in train_loader:
-            X_batch, y_batch, s_batch = X_batch.to(device), y_batch.to(device), s_batch.to(device)
+            #X_batch, y_batch, s_batch = X_batch.to(device), y_batch.to(device), s_batch.to(device)
 
             # Forward pass
             outputs = model(X_batch)
+            #restore greadients to zero
             optimizer.zero_grad()
             
             # Calculate loss
-            if loss_function == 'BCE':
-                loss = criterion(outputs, y_batch)
-            else:
-                #! change when cleaned up functions
-                fair_loss, _, _, _ = criterion(outputs, s_batch, y_batch, 0.0, 1.0)
-                bce_loss = criterion_bce(outputs, y_batch)
-                loss = (1.0 - balance_fair_BCE) * bce_loss + balance_fair_BCE * fair_loss
+            loss = calculate_loss(outputs, y_batch, s_batch, criterion, criterion_bce, loss_function, balance_fair_BCE)
 
             # Backward pass and optimize
             loss.backward()
@@ -135,17 +129,11 @@ def train_and_return_LSTM(X_train, y_train, s_train, loss_function,
             val_epoch_loss = 0.0
             with torch.no_grad():
                 for X_val_batch, y_val_batch, s_val_batch in val_loader:
-                    X_val_batch, y_val_batch, s_val_batch = X_val_batch.to(device), y_val_batch.to(device), s_val_batch.to(device)
+                    #X_val_batch, y_val_batch, s_val_batch = X_val_batch.to(device), y_val_batch.to(device), s_val_batch.to(device)
                     val_outputs = model(X_val_batch)
                     
                     # Calculate validation loss
-                    if loss_function == 'BCE':
-                        val_loss = criterion(val_outputs, y_val_batch)
-                    else:
-                        #! change when cleaned up functions
-                        fair_val_loss, _, _, _ = criterion(val_outputs, s_val_batch, y_val_batch, 0.0, 1.0)
-                        bce_val_loss = criterion_bce(val_outputs, y_val_batch)
-                        val_loss = (1.0 - balance_fair_BCE) * bce_val_loss + balance_fair_BCE * fair_val_loss
+                    val_loss = calculate_loss(val_outputs, y_val_batch, s_val_batch, criterion, criterion_bce, loss_function, balance_fair_BCE)
                     
                     val_epoch_loss += val_loss.item()
             
@@ -163,7 +151,7 @@ def train_and_return_LSTM(X_train, y_train, s_train, loss_function,
             # Early stopping
             if epochs_without_improvement >= patience:
                 print(f"Early stopping at epoch {epoch+1}, no improvement in validation loss.")
-                break
+                early_stopping = True
 
         lr_scheduler.step(avg_train_loss)
 
@@ -184,3 +172,23 @@ def train_and_return_LSTM(X_train, y_train, s_train, loss_function,
         return model, losses, val_losses
     else:
         return model
+    
+def calculate_loss(outputs, y_batch, s_batch, criterion, criterion_bce, loss_function, balance_fair_BCE):
+    if loss_function == 'BCE':
+        return criterion(outputs, y_batch)
+    #! change this when loss functions cleaned up
+    fair_loss, _, _, _ = criterion(outputs, s_batch, y_batch, 0.0, 1.0)
+    bce_loss = criterion_bce(outputs, y_batch)
+    return (1.0 - balance_fair_BCE) * bce_loss + balance_fair_BCE * fair_loss
+
+
+def prepare_data_loaders(X_train, y_train, s_train, X_val, y_val, s_val, batch_size):
+    train_dataset = TensorDataset(X_train, y_train, s_train)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    
+    if X_val is not None and y_val is not None and s_val is not None:
+        val_dataset = TensorDataset(X_val, y_val, s_val)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    else:
+        val_loader = None
+    return train_loader, val_loader
