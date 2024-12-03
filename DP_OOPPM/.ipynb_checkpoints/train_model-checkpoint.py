3c1d17bc67e3ec144c8083afc13111b084b6abe0
Jari Peeperkorn <jari.peeperkorn@gmail.com>
2024-12-03 16:18:30 +0100

    ignore

---
+++ DP_OOPPM/.ipynb_checkpoints/train_model-checkpoint.py
@@ -0,0 +1,199 @@
+import DP_OOPPM.predictive_models as predictive_models
+import DP_OOPPM.custom_loss_functions as custom_loss_functions
+
+
+import math
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+
+import numpy as np
+
+
+
+#! check if standard values make sense
+def train_and_return_LSTM(X_train, seq_len_train, y_train, s_train, 
+                          loss_function, 
+                          vocab_sizes, embed_sizes=None, num_numerical_features=None, 
+                          dropout=0.2, lstm_size=64, num_lstm=1, bidirectional = False,
+                          max_length=8, 
+                          learning_rate=0.001, max_epochs=300, batch_size=128,
+                          patience=10, 
+                          get_history=False, 
+                          X_val=None, seq_len_val=None, y_val=None, s_val=None,
+                          balance_fair_BCE = 0.1):
+
+
+    if embed_sizes == None:
+        #Default equal to square root
+        embed_sizes = [math.ceil(math.sqrt(vocab_sizes[i])) for i in range(len(vocab_sizes))]
+    if num_numerical_features == None:
+        num_numerical_features = 0
+    
+    
+    # Setting up GPU 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("device:", device)
+
+    # Define the model
+    model = predictive_models.LSTM_Model(vocab_sizes=vocab_sizes, embed_sizes=embed_sizes, 
+                                         num_numerical_features=num_numerical_features, max_length=max_length, 
+                                         dropout=dropout, lstm_size=lstm_size, num_lstm=num_lstm, bidirectional=bidirectional)
+
+    # Assign to device
+    model.to(device)
+    
+    #set model to train
+    model.train()
+
+    #Pick a loss function
+    if loss_function == 'dp':
+        criterion = custom_loss_functions.dp_reg()
+    elif loss_function == 'diff_abs':
+        criterion = custom_loss_functions.diff_abs_reg()
+    elif loss_function == 'wasserstein':
+        criterion = custom_loss_functions.wasserstein_reg()
+    elif loss_function == 'KL_divergence':
+        criterion = custom_loss_functions.KL_divergence_reg()
+    elif loss_function == 'diff_quadr':
+        criterion = custom_loss_functions.diff_quadr_reg()
+    elif loss_function == 'histogram':
+        criterion = custom_loss_functions.histogram_reg()
+    elif loss_function == 'histogram_sq':
+        criterion = custom_loss_functions.histogram_sq_reg()
+    elif loss_function == 'BCE':
+        criterion = torch.nn.BCELoss()
+    else:
+        print('No correct loss function given, defaulted to binary cross entropy')
+        loss_function = 'BCE'
+        criterion = torch.nn.BCELoss()
+    
+    #if fair loss we also use bce as balance
+    criterion_bce = torch.nn.BCELoss()
+
+    # Optimizer and scheduler used by benchmark
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode='min', factor=0.75, patience=10, threshold=0.001,
+        threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08
+    )
+
+    # Create DataLoader for training and validation
+    train_loader, val_loader = prepare_data_loaders(X_train, seq_len_train, y_train, s_train, X_val, seq_len_val, y_val, s_val, batch_size)
+
+    # Early stopping variables
+    best_val_loss = np.inf
+    epochs_without_improvement = 0
+    early_stopping = False
+
+    losses = []
+    val_losses = []
+
+    # Store the best model state
+    best_model_state = None
+
+    #load data to device only once
+    #X_train, seq_len_train, y_train, s_train = X_train.to(device), seq_len_train.to(device), y_train.to(device), s_train.to(device)
+    #if X_val is not None and y_val is not None and s_val is not None:
+    #    X_val, seq_len_val, y_val, s_val = X_val.to(device), seq_len_val.to(device), y_val.to(device), s_val.to(device)
+
+    for epoch in range(max_epochs):
+        if early_stopping:
+            break
+        # Set model to training mode each epoch
+        model.train()
+
+        epoch_loss = 0.0
+
+        # Training loop over each batch
+        for X_batch, seq_len_batch, y_batch, s_batch in train_loader:
+            X_batch, seq_len_batch, y_batch, s_batch = X_batch.to(device), seq_len_batch.to(device), y_batch.to(device), s_batch.to(device)
+
+            # Forward pass
+            outputs = model(X_batch, seq_len_batch)
+            #restore greadients to zero
+            optimizer.zero_grad()
+            
+            # Calculate loss
+            loss = calculate_loss(outputs, y_batch, s_batch, criterion, criterion_bce, loss_function, balance_fair_BCE)
+
+            # Backward pass and optimize
+            loss.backward()
+            optimizer.step()
+
+            epoch_loss += loss.item()
+
+        # Track average epoch loss
+        avg_train_loss = epoch_loss / len(train_loader)
+        losses.append(avg_train_loss)
+
+
+        # Validation step if validation data is provided
+        if X_val is not None and seq_len_val is not None and y_val is not None and s_val is not None:
+            model.eval()
+            val_epoch_loss = 0.0
+            with torch.no_grad():
+                for X_val_batch, seq_len_val_batch, y_val_batch, s_val_batch in val_loader:
+                    # Move tensors to device
+                    X_val_batch, seq_len_val_batch, y_val_batch, s_val_batch = X_val_batch.to(device), seq_len_val_batch.to(device), y_val_batch.to(device), s_val_batch.to(device)
+                    val_outputs = model(X_val_batch, seq_len_val_batch)
+                    
+                    # Calculate validation loss
+                    val_loss = calculate_loss(val_outputs, y_val_batch, s_val_batch, criterion, criterion_bce, loss_function, balance_fair_BCE)
+                    
+                    val_epoch_loss += val_loss.item()
+            
+            avg_val_loss = val_epoch_loss / len(val_loader)
+            val_losses.append(avg_val_loss)
+
+            # Check for improvement in validation loss
+            if avg_val_loss < best_val_loss:
+                best_val_loss = avg_val_loss
+                epochs_without_improvement = 0
+                best_model_state = model.state_dict()
+            else:
+                epochs_without_improvement += 1
+
+            # Early stopping
+            if epochs_without_improvement >= patience:
+                print(f"Early stopping at epoch {epoch+1}, no improvement in validation loss.")
+                early_stopping = True
+
+        lr_scheduler.step(avg_train_loss)
+
+        # Print progress
+        if (epoch + 1) % 10 == 0:
+            print(f"Epoch [{epoch+1}/{max_epochs}], Loss: {avg_train_loss:.4f}")
+            if X_val is not None and y_val is not None:
+                print(f"Validation Loss: {avg_val_loss:.4f}")
+            print(f"Learning rate: {optimizer.param_groups[0]['lr']:.6f}")
+        
+    # Load the best model state
+    if best_model_state is not None:
+        model.load_state_dict(best_model_state)
+    
+    model.eval()
+
+    if get_history:
+        return model, losses, val_losses
+    else:
+        return model
+    
+def calculate_loss(outputs, y_batch, s_batch, criterion, criterion_bce, loss_function, balance_fair_BCE):
+    if loss_function == 'BCE':
+        return criterion(outputs, y_batch)
+    #! change this when loss functions cleaned up
+    fair_loss, _, _, _ = criterion(outputs, s_batch, y_batch, 0.0, 1.0)
+    bce_loss = criterion_bce(outputs, y_batch)
+    return (1.0 - balance_fair_BCE) * bce_loss + balance_fair_BCE * fair_loss
+
+
+def prepare_data_loaders(X_train, seq_len_train, y_train, s_train, X_val, seq_len_val, y_val, s_val, batch_size):
+    train_dataset = TensorDataset(X_train, seq_len_train, y_train, s_train)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    
+    if X_val is not None and seq_len_val is not None and y_val is not None and s_val is not None:
+        val_dataset = TensorDataset(X_val, seq_len_val, y_val, s_val)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    else:
+        val_loader = None
+    return train_loader, val_loader
---
+++ Preprocessing/.ipynb_checkpoints/log_preparation_specific-checkpoint.py
@@ -0,0 +1,312 @@
+import Preprocessing.encode_categorical_data as encode_categorical_data
+import Preprocessing.split_data as split_data
+import Preprocessing.get_prefix_label_pairs as get_prefix_label_pairs
+
+from sklearn.preprocessing import MinMaxScaler
+
+import numpy as np
+
+#! add settings other log types
+
+
+# Define for the used event logs, which are categorical, which are numerical and change order etc.
+def prepare_log(df, log_name, max_prefix_len, test_fraction=0.3, return_valdiation_set = False, validation_fraction=0.1, act_label = 'concept:name', case_id='case:concept:name', sensitive_column = 'case:gender', drop_sensitive=False):
+    
+    #added to be sure later on
+    df = sort_log(df)
+    
+    if log_name == "hiring":
+        #add outcome label
+        df = add_label(df, "hiring", act_label, case_id)
+
+        #REMOVE EVENT THAT COME AFTER THE OUTCOME EVENT
+        df = df[df['valid_for_prefix'] == 1]
+
+        #order features
+        df, num_numerical_features, true_num_feature_list = clean_order_features(df, "hiring")
+        #encode features
+        df, vocsizes = encode_features(df, "hiring")
+
+
+    elif log_name == "hospital":
+        #add outcome label
+        df = add_label(df, "hospital", act_label, case_id)
+
+        #REMOVE EVENT THAT COME AFTER THE OUTCOME EVENT
+        df = df[df['valid_for_prefix'] == 1]
+
+        #order features
+        df, num_numerical_features, true_num_feature_list  = clean_order_features(df, "hospital")
+        #encode features
+        df, vocsizes = encode_features(df, "hospital")
+
+
+    elif log_name == "lending":
+        #add outcome label
+        df = add_label(df, "lending", act_label, case_id)
+
+        #REMOVE EVENT THAT COME AFTER THE OUTCOME EVENT
+        df = df[df['valid_for_prefix'] == 1]
+
+        #order features
+        df, num_numerical_features, true_num_feature_list  = clean_order_features(df, "lending")
+        #encode features
+        df, vocsizes = encode_features(df, "lending")
+
+       
+    elif log_name == "renting":
+        #add outcome label
+        df = add_label(df, "renting", act_label, case_id)
+
+        #REMOVE EVENT THAT COME AFTER THE OUTCOME EVENT
+        df = df[df['valid_for_prefix'] == 1]
+
+        #order features
+        df, num_numerical_features, true_num_feature_list  = clean_order_features(df, "renting")
+        #encode features
+        df, vocsizes = encode_features(df, "renting")
+
+       
+    else:
+        #todo: delete names of logs we didn't use
+        raise ValueError("No valid event log type (of currently implemented) logs was given, try hiring, hospital, lending or renting.")
+    
+        
+    tr, te = split_data.train_test_split(df, test_fraction=test_fraction)
+
+
+    #scale the true numerical data
+    scaler = MinMaxScaler()
+    tr_old_shape = tr[true_num_feature_list].shape
+    tr[true_num_feature_list] = scaler.fit_transform(tr[true_num_feature_list].to_numpy().reshape(-1,1)).reshape(tr_old_shape)
+    te_old_shape = te[true_num_feature_list].shape
+    te[true_num_feature_list] = scaler.transform(te[true_num_feature_list].to_numpy().reshape(-1,1)).reshape(te_old_shape)
+
+    # todo add possiblitiy validation set split here?
+
+
+    #in this function the case_ID is droppend anyway
+    trval_X, trval_y, trval_s, updated_max_prefix_length = get_prefix_label_pairs.create_pairs_train_sensitive(df=tr, max_prefix_length=max_prefix_len, sensitive_column=sensitive_column, drop_sensitive=drop_sensitive, case_id=case_id, outcome='outcome')
+    te_X, te_y, te_s = get_prefix_label_pairs.create_pairs_test_sensitive(df=te, max_prefix_length=updated_max_prefix_length, sensitive_column=sensitive_column, drop_sensitive=drop_sensitive, case_id=case_id, outcome='outcome')
+
+    if return_valdiation_set == False:
+        return trval_X, trval_y, trval_s, te_X, te_y, te_s, vocsizes, num_numerical_features
+    
+    else:
+        #split off validation set
+        val_split_point = int(len(trval_y)*(1.0-validation_fraction))
+        tr_X = trval_X[:val_split_point]
+        tr_y = trval_y[:val_split_point]
+        tr_s = trval_s[:val_split_point]
+        val_X = trval_X[val_split_point:]
+        val_y = trval_y[val_split_point:]
+        val_s = trval_s[val_split_point:]
+        return tr_X, tr_y, tr_s, val_X, val_y, val_s, te_X, te_y, te_s, vocsizes, num_numerical_features
+
+
+#!to do: add function that deletes cases from label definition onwards?
+
+def add_label(df, log_name, act_label = 'concept:name', case_id='case:concept:name'):
+    """
+    Add outcome column to DataFrame based on a certain definition for different log types.
+
+    Parameters:
+    df (pd.DataFrame): Input DataFrame representing the event log.
+    log_name (str): Type of log, e.g., "hiring", "hospital", "lending", or "renting".
+    act_label (str): Column name for activity labels in the DataFrame (default is 'concept:name').
+    case_id (str): Column name for case identifiers in the DataFrame (default is 'case:concept:name').
+
+    Returns:
+    pd.DataFrame: New DataFrame with an additional column indicating the presence of a specific activity based on the log type.
+    """
+    if log_name == "hiring":
+        #we define the outcome based on whether make offer is present somewhere
+        print(f'Preprocess {log_name} type event log, with outcome defined on presence of activity Make Job Offer.')
+        new_df = add_label_activity_presence(df, act_label, case_id, 'Make Job Offer')
+
+    elif log_name == "hospital":
+        #! double check whether there are sets where both are present
+        #we define the outcome based on whether treatment unsuccesful or treatment unsuccesful is initially reached
+        print(f'Preprocess {log_name} type event log, with outcome defined on presence of treatment succesful')
+        new_df = add_label_activity_presence(df, act_label, case_id, 'Treatment succesful')
+    elif log_name == "lending":
+        #we define the outcome based on whether the loan agreement is signed
+        print(f'Preprocess {log_name} type event log, with outcome defined on presence of sign loan agreement')
+        new_df = add_label_activity_presence(df, act_label, case_id, 'Sign Loan Agreement')
+    elif log_name == "renting":
+        #! we probably need to delete all events after sign contract
+        #we define the outcome based on whether the contract is signed or proscpective tenant is rejected
+        print(f'Preprocess {log_name} type event log, with outcome defined on presence of sign contract')
+        new_df = add_label_activity_presence(df, act_label, case_id, 'Sign Contract')
+    else:
+        #todo: delete names of logs we didn't use
+        raise ValueError("No valid event log type (of currently implemented) logs was given, try hiring, hospital, lending or renting.")
+    return new_df
+    
+
+def add_label_activity_presence(df, act_label, case_id, outcome_act):
+    '''
+    Add a new column to the DataFrame indicating whether each row's case ID matches the specified outcome action label,
+    and include only activities up to but not including the first occurrence of the outcome action.
+    
+    Params:
+    - df: DataFrame containing the data
+    - act_label: Column name for the action label
+    - case_id: Column name for the case ID
+    - outcome_act: Value of the outcome action
+    
+    Returns:
+    - DataFrame with additional 'outcome' and 'valid_for_prefix' columns
+    '''
+    # Identify case IDs where act_label is equal to outcome_act
+    outcome_cases = df.loc[df[act_label] == outcome_act, case_id].unique()
+    # Add a new 'outcome' column where the value is 1 if case_id is in outcome_cases, otherwise 0
+    df['outcome'] = df[case_id].apply(lambda x: 1 if x in outcome_cases else 0)
+
+    # Initialize 'valid_for_prefix' to mark activities before the outcome event in each case
+    df['valid_for_prefix'] = 1  # Initialize all as valid initially
+
+    # Iterate through each case and mark activities up to the first outcome event
+    for case in outcome_cases:
+        outcome_index = df[(df[case_id] == case) & (df[act_label] == outcome_act)].index
+        if not outcome_index.empty:
+            first_outcome_index = outcome_index[0]
+            # Mark only activities before the first outcome event as valid
+            df.loc[(df[case_id] == case) & (df.index >= first_outcome_index), 'valid_for_prefix'] = 0
+
+    return df
+
+def clean_order_features(df, log_name):
+    #todo ORDER FEATURES, REMOVE DUPLICATES, CONVERT BOOLEAN TO NUMERICAL, MAKE SURE OUTCOME IS LAST
+    if log_name == "hiring":
+        #! we delete time for now
+        categorical_features = ['concept:name', 'resource']
+        binary_features = ['case:german speaking', 'case:gender', 'case:citizen', 'case:protected', 'case:religious']
+        num_features = ['case:age', 'case:yearsOfEducation']
+        num_numerical_features = len(binary_features) + len(num_features)
+        for col in binary_features:
+            #convert to integers instead of booleans
+            df[col] = df[col].astype(int)
+        #we add the case id in case we need it later again
+        cols_order_filtered = categorical_features + binary_features + num_features + ['case:concept:name', 'outcome']
+        df = df[cols_order_filtered]
+
+    elif log_name == "hospital":
+        #! we delete time for now
+        categorical_features = ['concept:name', 'resource']
+        binary_features = ['case:german speaking', 'case:private_insurance', 'case:underlying_condition', 'case:gender', 'case:citizen', 'protected']
+        num_features = ['case:age']
+        num_numerical_features = len(binary_features) + len(num_features)
+        for col in binary_features:
+            #convert to integers instead of booleans
+            df[col] = df[col].astype(int)
+        #we add the case id in case we need it later again
+        cols_order_filtered = categorical_features + binary_features + num_features + ['case:concept:name', 'outcome']
+        df = df[cols_order_filtered]
+   
+
+    elif log_name == "lending":
+        #! we delete time for now
+        categorical_features = ['concept:name', 'resource']
+        binary_features = ['case:german speaking', 'case:gender', 'case:citizen', 'case:protected']
+        num_features = ['case:age', 'case:yearsOfEducation', 'case:CreditScore']
+        num_numerical_features = len(binary_features) + len(num_features)
+        for col in binary_features:
+            #convert to integers instead of booleans
+            df[col] = df[col].astype(int)
+        #we add the case id in case we need it later again
+        cols_order_filtered = categorical_features + binary_features + num_features + ['case:concept:name', 'outcome']
+        df = df[cols_order_filtered]
+
+    elif log_name == "renting":
+        #! we delete time for now
+        categorical_features = ['concept:name', 'resource']
+        binary_features = ['case:german speaking', 'case:gender', 'case:citizen', 'case:protected', 'case:married']
+        num_features = ['case:age', 'case:yearsOfEducation']
+        num_numerical_features = len(binary_features) + len(num_features)
+        for col in binary_features:
+            #convert to integers instead of booleans
+            df[col] = df[col].astype(int)
+        #we add the case id in case we need it later again
+        cols_order_filtered = categorical_features + binary_features + num_features + ['case:concept:name', 'outcome']
+        df = df[cols_order_filtered]
+
+    
+    else:
+        #todo: delete names of logs we didn't use
+        raise ValueError("No valid event log type (of currently implemented) logs was given, try hiring, hospital, lending or renting.")
+    
+    #we also return list of true numericals to know which to minmaxscale
+    return df, num_numerical_features, num_features 
+
+
+
+def encode_features(df, log_name):
+    voc_sizes = []
+    if log_name == "hiring":
+        cat_features = ['concept:name', 'resource']
+        for cat in cat_features:
+            vocsize = df[cat].nunique()
+            voc_sizes.append(vocsize)
+        new_df = encode_categorical_data.integer_encode_categorical_data(df, cat_features)
+
+    elif log_name == "hospital":
+        # todo!
+        #aanvullen?
+        cat_features = ['concept:name', 'resource']
+        for cat in cat_features:
+            vocsize = df[cat].nunique()
+            voc_sizes.append(vocsize)
+        new_df = encode_categorical_data.integer_encode_categorical_data(df, cat_features)
+
+    elif log_name == "lending":
+        # todo!
+        #aanvullen?
+        cat_features = ['concept:name', 'resource']
+        for cat in cat_features:
+            vocsize = df[cat].nunique()
+            voc_sizes.append(vocsize)
+        new_df = encode_categorical_data.integer_encode_categorical_data(df, cat_features)
+
+    elif log_name == "renting":
+        # todo!
+        #aanvullen?
+        cat_features = ['concept:name', 'resource']
+        for cat in cat_features:
+            vocsize = df[cat].nunique()
+            voc_sizes.append(vocsize)
+        new_df = encode_categorical_data.integer_encode_categorical_data(df, cat_features)
+
+    else:
+        #todo: delete names of logs we didn't use
+        raise ValueError("No valid event log type (of currently implemented) logs was given, try hiring, hospital, lending or renting.")
+    
+    return new_df, voc_sizes
+
+
+def sort_log(df, case_id = 'case:concept:name', timestamp = 'time:timestamp'):
+    """Sort events in event log such that cases that occur first are stored 
+    first, and such that events within the same case are stored based on timestamp. 
+
+    Parameters
+    ----------
+    df : _type_
+        _description_
+    case_id : str, optional
+        _description_, by default 'case:concept:name'
+    timestamp : str, optional
+        _description_, by default 'time:timestamp'
+    act_label : str, optional
+        _description_, by default 'concept:name'
+    """
+    df_help = df.sort_values([case_id, timestamp], ascending = [True, True], kind='mergesort')
+    # Now take first row of every case_id: this contains first stamp 
+    df_first = df_help.drop_duplicates(subset = case_id)[[case_id, timestamp]]
+    df_first = df_first.sort_values(timestamp, ascending = True, kind='mergesort')
+    # Include integer index to sort on. 
+    df_first['case_id_int'] = [i for i in range(len(df_first))]
+    df_first = df_first.drop(timestamp, axis = 1)
+    df = df.merge(df_first, on = case_id, how = 'left')
+    df = df.sort_values(['case_id_int', timestamp], ascending = [True, True], kind='mergesort')
+    df = df.drop('case_id_int', axis = 1)
+    return df 
